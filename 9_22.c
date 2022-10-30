2df96bd45d3101757b8ade92a3cd61555b2fa30b
L96703 <851030769@qq.com>
2022-10-30 23:43:04 +0800

    记录

---
+++ 9_22.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+int main()
+{
+	int a=0;
+	int b=1;
+	int sum;
+	scanf("%d%d", &a, &b);
+	sum = a+b;
+	printf("sum = %d\n", sum);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 9_25.c
@@ -0,0 +1,55 @@
+/*#include<stdio.h>
+
+int Max(int x, int y)
+{
+	if( x > y)
+	return x;
+	else
+	return y;
+}
+int main()
+{
+	int num1 = 5;
+	int num2 = 10;
+	int max = 0;
+	max = Max(num1,num2);
+	printf("max = %d\n",max);
+	return 0;
+}
+*/
+/*
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = ~a;
+	printf ("%d\n",b);
+	return 0;
+}
+*/
+/*
+#include<stdio.h>
+
+int main()
+{
+	int a = (int)3.14;
+	return 0;
+}
+*/
+
+
+#include<stdio.h>
+int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+
+int main()
+{
+	int a = 10, b = 20;
+	int sum = Add(a,b);
+	
+	return 0;
+}
